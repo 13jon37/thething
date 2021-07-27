4af2d1f8ad40080ac34c9eed68cd36a54071968a
13jon37 <jonlockllyr2@gmail.com>
2021-07-27 16:13:40 -0400

    player animation

--- src/player.rs
+++ src/player.rs
@@ -18,26 +18,33 @@ pub struct Player<'a> {
     direction: Direction,
     moving: bool,
     jumping: bool,
+    current_frame: i32,
 }
 
 impl<'a> Player<'a> {
     pub fn new<T>(creator: &'a TextureCreator<T>, position: Point<i32>, size: Point<u32>, dy: i32) -> Self {
         Self {
-            texture: load_texture("Assets/grass.png", &creator),
+            texture: load_texture("Assets/player.png", &creator),
             position: position,
             size: size,
             dy: dy,
             direction: Direction::Right,
             moving: false,
             jumping: false,
+            current_frame: 0,
         }
     }
 
-    fn animate(&self) {
-        println!("Animate!");
+    fn sprite_direction(&self, direction: &Direction) -> i32 {
+        use self::Direction::*;
+        match direction {
+            Left => 1,
+            Right => 2,
+        }
     }
 
     pub fn input_and_update(&mut self, e: &sdl2::EventPump) {
+        /* Input Code */
         if e.keyboard_state().is_scancode_pressed(Scancode::A) {
             self.moving = true;
             self.direction = Direction::Left;
@@ -50,9 +57,26 @@ impl<'a> Player<'a> {
         }
         if e.keyboard_state().is_scancode_pressed(Scancode::Space) {
             self.jumping = true;
-            self.moving = true;
+            //self.moving = true;
             self.position.y -= self.dy * 2; // Jump
         }
+
+         /* Update/Animate Code */
+        if self.moving {
+            self.current_frame = (self.current_frame + 1) % 3;
+        }
+        if !self.moving {
+            // Set player to the idle standing sprite
+            match self.direction {
+                Direction::Left  => {
+                    self.current_frame = 0;
+                },
+                Direction::Right => {
+                    self.current_frame = 0;
+                }
+            }
+        }
+        
         self.position.y += self.dy; // Gravity
         self.moving = false;
         self.jumping = false;
@@ -64,12 +88,16 @@ impl<'a> EntityDefault for Player<'a> {
     fn update(&mut self) {}
 
     fn render(&self, canvas: &mut WindowCanvas) -> Result<(), String> {
-        /*canvas.set_draw_color(Color::RGB(255, 255, 255));
-        canvas.fill_rect(Rect::new(self.position.x, self.position.y, 
-        self.size.x, self.size.y))?;*/
-        self.animate();
-        let dst_rect = Rect::new(self.position.x, self.position.y, self.size.x, self.size.y);
-        canvas.copy(&self.texture, None, dst_rect)?;
+        let src_rect = Rect::new(
+             0 + 16 * self.current_frame,
+             0 + 16 * self.sprite_direction(&self.direction),
+            16, 16
+        );
+        let dst_rect = Rect::new(self.position.x, 
+                                      self.position.y,
+                                self.size.x,
+                               self.size.y);
+        canvas.copy(&self.texture, src_rect, dst_rect)?;
 
         Ok(())
     }
